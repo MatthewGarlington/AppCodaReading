18e023a9d501451a6de824c36b5265f4b096e367
Matthew Garlington <SBMatthewG@gmail.com>
2021-01-24 19:04:50 -0500

    Initial Commit

---
+++ AppCodaPractice/AppCodaPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCodaPracticeApp.swift
+//  AppCodaPractice
+//
+//  Created by Matthew Garlington on 1/24/21.
+//
+
+import SwiftUI
+
+@main
+struct AppCodaPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
