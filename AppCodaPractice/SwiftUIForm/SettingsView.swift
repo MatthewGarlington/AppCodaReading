cbda517a50dab5080503e475b212cd9b2349100d
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-25 10:59:30 -0400

    Added view to showcase forms, pickers, and steppers in SwiftUI

--- AppCodaPractice/SwiftUIForm/SettingsView.swift
+++ AppCodaPractice/SwiftUIForm/SettingsView.swift
@@ -7,14 +7,55 @@
 
 import SwiftUI
 
-struct SettingsView: View {
+struct FormSettingView: View {
+    private var displayOrders = [ "Alphabetical", "Show Favorite First", "Show Check-in First"]
+    @State private var selectedOrder = 0
+    @State private var showCheckInOnly = false
+    @State private var maxPriceMaxLevel = 5
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section(header: Text("SORT PREFERENCE")) {
+                    Picker(selection: $selectedOrder, label: Text("Display Order")) {
+                        ForEach(0..<displayOrders.count, id: \.self) {
+                            Text(self.displayOrders[$0])
+                        }
+                    }
+                }
+
+                Section(header: Text("FILTER PREFERENCE")) {
+                    Toggle(isOn: $showCheckInOnly, label: {
+                        Text("Show Check In Only")
+                    })
+
+                    Stepper(
+                        onIncrement: {
+                            self.maxPriceMaxLevel += 1
+                            if self.maxPriceMaxLevel > 5 {
+                                self.maxPriceMaxLevel = 5
+                            }
+                        },
+                        onDecrement: {
+                            self.maxPriceMaxLevel -= 1
+
+                            if self.maxPriceMaxLevel < 1 {
+                                self.maxPriceMaxLevel = 1
+                            }
+
+                        },
+                        label: {
+                            Text("Show \(String(repeating:"$", count: maxPriceMaxLevel)) or below")
+                        })
+                }
+            }
+
+            .navigationBarTitle("Settings")
+        }
     }
 }
-
-struct SettingsView_Previews: PreviewProvider {
+struct FormSettingsView_Previews: PreviewProvider {
     static var previews: some View {
-        SettingsView()
+        FormSettingView()
     }
 }
