efd836a124e3680f957c6c613029da4e286fb832
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-25 10:35:16 -0400

    Added View to showcase SwiftUI Transitions
    The added view displays a transition with an offest and scale effect when the square is tapped

--- AppCodaPractice/WorkingWithTransitions.swift
+++ AppCodaPractice/WorkingWithTransitions.swift
@@ -8,8 +8,92 @@
 import SwiftUI
 
 struct WorkingWithTransitions: View {
+    @State private var show = false
+    
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+ 
+        VStack {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 300, height: 300)
+                .foregroundColor(.green)
+                .overlay(
+                    Text("Show details")
+                        .font(.system(.largeTitle, design: .rounded))
+                        .bold()
+                        .foregroundColor(.white)
+                    
+                )
+            
+            
+            if show {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 300, height: 300)
+                .foregroundColor(.purple)
+                .overlay(
+                    Text("Well, here is the details")
+                        .font(.system(.largeTitle, design: .rounded))
+                        .bold()
+                        .foregroundColor(.white)
+                ).transition(.offsetScaleOpacity)
+            }
+        }
+        
+        .onTapGesture {
+            withAnimation(Animation.spring()) {
+                
+                self.show.toggle()
+            }
+        }
+        VStack {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 300, height: 300)
+                .foregroundColor(.green)
+                .overlay(
+                    Text("Show details")
+                        .font(.system(.largeTitle, design: .rounded))
+                        .bold()
+                        .foregroundColor(.white)
+                    
+                )
+            
+            
+            if show {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 300, height: 300)
+                .foregroundColor(.purple)
+                .overlay(
+                    Text("Well, here is the details")
+                        .font(.system(.largeTitle, design: .rounded))
+                        .bold()
+                        .foregroundColor(.white)
+                ).transition(.scaleAndOffset)
+            }
+        }
+        
+        .onTapGesture {
+            withAnimation(Animation.spring()) {
+                
+                self.show.toggle()
+            }
+        }
+    }
+}
+
+// Extension for offSetScaleOpacity Transition
+extension AnyTransition {
+    static var offsetScaleOpacity: AnyTransition {
+        AnyTransition.offset(x: -600, y: 0).combined(with: .scale).combined(with: .opacity)
+    }
+}
+// scale with an offset transition
+extension AnyTransition {
+    static var scaleAndOffset: AnyTransition {
+        AnyTransition.asymmetric(
+            insertion: .scale(scale: 0, anchor: .bottom),
+            removal: .offset(x: -600, y: 00)
+        )
     }
 }
 
