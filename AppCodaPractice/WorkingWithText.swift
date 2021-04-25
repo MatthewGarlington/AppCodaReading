cfc78416b42e276087561f289eaf4dd52286a3c8
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-25 10:36:30 -0400

    Added View for SwiftUI Text
    The added text view displays text with a foreground color, rotation effect if enabled, and examples of truncation on longer text and font size

--- AppCodaPractice/WorkingWithText.swift
+++ AppCodaPractice/WorkingWithText.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct WorkingWithText: View {
     var body: some View {
  
         Text("Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma—which is living with the results of other people’s thinking. Don’t let the noise of others’ opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition.")
@@ -26,8 +26,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct WorkingWithText_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        WorkingWithText()
     }
 }
