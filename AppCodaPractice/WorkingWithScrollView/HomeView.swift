a713aa73b07a31ce697c31f6066ccde595026627
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-25 10:44:28 -0400

    Added View to Display Horizontal Scrolling and Vertical Scrolling in SwiftUI

--- AppCodaPractice/WorkingWithScrollView/HomeView.swift
+++ AppCodaPractice/WorkingWithScrollView/HomeView.swift
@@ -9,12 +9,88 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView {
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    
+                    Text("Monday, Jan. 25".uppercased())
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                    
+                    Text("Your Reading".uppercased())
+                        .font(.system(.largeTitle, design: .rounded))
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                       
+         
+                    
+                }.padding(.horizontal)
+                    Spacer()
+            }
+            
+            VStack {
+                CardView(image: "swiftuibutton", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Simon Ng")
+                CardView(image: "macos", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel Theodoropoulos")
+                CardView(image: "flutter", category: "Flutter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
+                CardView(image: "naturallanguage", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
+            }
+        }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+    }
+}
+
+
+struct HomeViewHorizontal: View {
+    var body: some View {
+        
+        ScrollView(.horizontal, showsIndicators: false) {
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    
+                    Text("Monday, Jan. 25".uppercased())
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                    
+                    Text("Your Reading".uppercased())
+                        .font(.system(.largeTitle, design: .rounded))
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                       
+         
+                    
+                }.padding(.horizontal)
+                    Spacer()
+            }
+            
+            HStack {
+                
+                Group {
+               
+                CardView(image: "swiftuibutton", category: "SwiftUI", heading: "Drawing a Border with Rounded Corners", author: "Simon Ng")
+             
+                CardView(image: "macos", category: "macOS", heading: "Building a Simple Editing App", author: "Gabriel Theodoropoulos")
+                 
+                CardView(image: "flutter", category: "Flutter", heading: "Building a Complex Layout with Flutter", author: "Lawrence Tan")
+           
+                CardView(image: "naturallanguage", category: "iOS", heading: "What's New in Natural Language API", author: "Sai Kambampati")
+                  
+                    
+                }     .frame(width: 300)
+            }
+        }
+    }
+}
+
+struct HomeViewHorizontal_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewHorizontal()
     }
 }
