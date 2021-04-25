9db6ddc34361ef0b763e542df80421db2f05fe58
Matthew Garlington <SBMatthewG@gmail.com>
2021-04-25 10:50:44 -0400

    Added View to Show how Modals work with SwiftUI
    The added button of the image displays a modal with a detail view of the selected image. A custom dismiss button is added with an alert presenting to ensure the user is done with the page they are viewing. This is done by removing Navigation View and creating a custom action to dimiss the model with in the alert

--- AppCodaPractice/WorkingWithModals.swift
+++ AppCodaPractice/WorkingWithModals.swift
@@ -8,13 +8,87 @@
 import SwiftUI
 
 struct WorkingWithModals: View {
+    
+    @State var showDetailView = false
+    @State var selectedArticle: Article?
+    
+ 
+    
+   
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(articles) { article in
+                ArticleRow(article: article)
+                
+                  .onTapGesture {
+                            self.showDetailView = true
+                            self.selectedArticle = article
+                        }
+            }
+            
+            // This creates a Full Screen model to show the detail view with a dismiss button instead of swipe 
+           
+            .fullScreenCover(item: self.$selectedArticle) { article in
+                ArticleDetailView(article: article)
+            }
+            
+            // This shows a regular modal view that is not full screen and can be dismissed with a swipe
+//            .sheet(isPresented: self.$showDetailView) {
+//
+//                    if let selectedArticle = self.selectedArticle {
+//                        ArticleDetailView(article: selectedArticle)
+//                    }
+//                }
+
+            .navigationBarTitle("Your Reading")
+            .navigationViewStyle(StackNavigationViewStyle())
+        }
     }
 }
 
 struct WorkingWithModals_Previews: PreviewProvider {
     static var previews: some View {
         WorkingWithModals()
+    }
+}
+
+struct ArticleRow: View {
+    var article: Article
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Image(article.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(5)
+            
+            Text(article.title)
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.black)
+                .lineLimit(3)
+                .padding(.bottom, 0)
+            
+            Text("By \(article.author)".uppercased())
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                .padding(.bottom, 0)
+                
+            HStack(spacing: 3) {
+                ForEach(1...(article.rating), id: \.self) { _ in
+                    Image(systemName: "star.fill")
+                        .font(.caption)
+                        .foregroundColor(.yellow)
+                }
+                
+                
+            }
+            
+            Text(article.excerpt)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            
+            
+        }
+     
     }
 }
